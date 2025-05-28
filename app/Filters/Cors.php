db62b07abc294a06abcc669582bb28f78277d4cb
HKT\PIRATCHAI.K <thai-it1@hanwa.co.jp>
2025-05-28 13:56:11 +0700

    fix: Improve CORS filter to handle OPTIONS requests and set appropriate headers

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -7,14 +7,17 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Cors implements FilterInterface
-{
-    public function before(RequestInterface $request, $arguments = null)
+{    public function before(RequestInterface $request, $arguments = null)
     {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization');
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+        header('Access-Control-Max-Age: 3600');
+        header('Access-Control-Allow-Credentials: true');
+        
         $method = $_SERVER['REQUEST_METHOD'];
         if ($method == "OPTIONS") {
+            header('HTTP/1.1 200 OK');
             die();
         }
     }
